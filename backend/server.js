5e18f1fabd6b696f270f438a5957dede6da8c956
Sushmitha <108257457+Sushmitha711@users.noreply.github.com>
2023-06-27 00:06:34 +0530

    Connected server to database

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,19 @@ const bodyparsel=require("body-parser");
 const mongoose=require("mongoose");
 
 const app=express();
+// middleware
 
-const PORT=process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyparsel.json());
+
+// Routes
+app.get("/",(req,res)=>{
+    res.send("Home page")
+})
 
 // Connectiong to mongodb to server
+const PORT=process.env.PORT || 5000;
 
 mongoose
 .connect(process.env.MONGO_URL)
