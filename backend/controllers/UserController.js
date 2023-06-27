be17d95a41c5fba200b0c57b6e3580e6c99f5599
Sushmitha <108257457+Sushmitha711@users.noreply.github.com>
2023-06-27 23:47:17 +0530

    Register Form Schema,Routing is create

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,7 @@
+const UserRegister=async(req,res)=>{
+    res.send("User register")
+}
+
+module.exports={
+    UserRegister
+}
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-const dotenv=require("dotenv").config();
-const express=require("express");
-const cors=require("cors");
-const bodyparsel=require("body-parser");
-const mongoose=require("mongoose");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyparsel = require("body-parser");
+const mongoose = require("mongoose");
+const UserRoutes = require("./routes/UserRoutes")
 
-const app=express();
+const app = express();
+
 // middleware
-
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyparsel.json());
 
+// Router Middleware
+app.use("/api/users", UserRoutes)
+
 // Routes
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Home page")
 })
 
 // Connectiong to mongodb to server
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
-.connect(process.env.MONGO_URL)
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server started at port ${PORT}`)
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started at port ${PORT}`)
+        })
     })
-})
-.catch((err)=>{console.log(err)})
+    .catch((err) => { console.log(err) })
